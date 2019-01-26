fb1d8bf0c9e6a045c6d8457bcbd43427355cd48e
Igor Ferreira <ipferreira@inf.ufrgs.br>
2019-01-26 20:25:19 -0200

    Suppressing NullPointerException from class

--- src/main/java/com/interceptors/stdout/SimpleClassTransformer.java
+++ src/main/java/com/interceptors/stdout/SimpleClassTransformer.java
@@ -1,6 +1,9 @@
 package com.interceptors.stdout;
 
-import javassist.*;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -13,7 +16,24 @@ public class SimpleClassTransformer implements ClassFileTransformer {
             final Class<?> classBeingRedefined,
             final ProtectionDomain protectionDomain,
             final byte[] classfileBuffer ) throws IllegalClassFormatException {
-        System.out.println("Visiting " + className);
+
+        if ("Main".equals(className)) {
+            try {
+                final ClassPool classPool = ClassPool.getDefault();
+                final CtClass clazz =
+                        classPool.get(className.replace("/", "."));
+
+                CtMethod method = clazz.getDeclaredMethod("main");
+                method.addCatch("{System.out.println(\"Suppressed exception\");return;}", classPool.get("java.lang.NullPointerException"));
+
+                byte[] byteCode = clazz.toBytecode();
+                clazz.detach();
+
+                return byteCode;
+            } catch (final Exception ex) {
+                ex.printStackTrace();
+            }
+        }
 
         return classfileBuffer;
     }
